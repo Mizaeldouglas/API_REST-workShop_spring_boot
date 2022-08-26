d660d68a523ede4f91b77677f2b3f23db4d27143
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-08-25 21:13:53 -0300

    Conectando ao MongoDB com repository e service

---
+++ work_shop_spring/src/main/java/oi/github/mizaeldouglas/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package oi.github.mizaeldouglas.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import oi.github.mizaeldouglas.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
+	
+	
+}
