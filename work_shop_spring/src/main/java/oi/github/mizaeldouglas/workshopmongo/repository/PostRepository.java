20206c7fd0a49d8b4335873ad968c9777bcff93e
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-09-01 10:56:54 -0300

    Consulta simples com @Query

--- work_shop_spring/src/main/java/oi/github/mizaeldouglas/workshopmongo/repository/PostRepository.java
+++ work_shop_spring/src/main/java/oi/github/mizaeldouglas/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package oi.github.mizaeldouglas.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import oi.github.mizaeldouglas.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import oi.github.mizaeldouglas.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
